7c733c4492e1213d53b6cf1fb7b6c9f0d8adf2d8
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-09-20 01:47:09 +0800

    added dashboard page

---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react'; 
+import Layout from '../components/Layout';
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <h1>Dashboard</h1>
+    </Layout>
+  );
+}
