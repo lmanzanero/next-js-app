429fb14a89504dbea9cf241db7391107fa8be38e
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-09-20 01:04:34 +0800

    added login and register routes

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react'; 
+import Layout from '../components/Layout';
+
+export default function Login() {
+  return (
+    <Layout>
+      <h1>Login</h1>
+    </Layout>
+  );
+}
---
+++ pages/register.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react'; 
+import Layout from '../components/Layout';
+
+export default function Register() {
+  return (
+    <Layout>
+      <h1>Register</h1>
+    </Layout>
+  );
+}
