015bb79af2c705c543ea0f417aab7cfcf999f7ab
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-02-24 02:30:27 +0800

    added basic design to home screen sections

--- ui/features_section.js
+++ ui/features_section.js
@@ -3,7 +3,70 @@ import React from 'react'
 export default function Features() {
   return (
     <div className="section">
-      Features
+      <div className="features-section">
+       <h2>Environmental Protection in your hands!</h2>
+        <div className="section-row">
+          <div className="feature-item">
+            <div className="feature-icon">
+              Icon
+            </div>
+            <div className="feature-info">
+              <div className="feature-title">Innovation</div>
+              <div className="feature-desc">Connect with experts and conncerned citizens to protect our enviornment</div>
+            </div>
+          </div>
+          <div className="feature-item">
+            <div className="feature-icon">
+              Icon
+            </div>
+            <div className="feature-info">
+              <div className="feature-title">Innovation</div>
+              <div className="feature-desc">Connect with experts and conncerned citizens to protect our enviornment</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="section-row">
+          <div className="feature-item">
+            <div className="feature-icon">
+              Icon
+            </div>
+            <div className="feature-info">
+              <div className="feature-title">Innovation</div>
+              <div className="feature-desc">Connect with experts and conncerned citizens to protect our enviornment</div>
+            </div>
+          </div>
+          <div className="feature-item">
+            <div className="feature-icon">
+              Icon
+            </div>
+            <div className="feature-info">
+              <div className="feature-title">Innovation</div>
+              <div className="feature-desc">Connect with experts and conncerned citizens to protect our enviornment</div>
+            </div>
+          </div>
+        </div>
+        <div className="section-row">
+          <div className="feature-item">
+            <div className="feature-icon">
+              Icon
+            </div>
+            <div className="feature-info">
+              <div className="feature-title">Innovation</div>
+              <div className="feature-desc">Connect with experts and conncerned citizens to protect our enviornment</div>
+            </div>
+          </div>
+          <div className="feature-item">
+            <div className="feature-icon">
+              Icon
+            </div>
+            <div className="feature-info">
+              <div className="feature-title">Innovation</div>
+              <div className="feature-desc">Connect with experts and conncerned citizens to protect our enviornment</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- ui/hero_section.js
+++ ui/hero_section.js
@@ -3,7 +3,19 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <div className="section">
-       <h1>EcoCrypt</h1>
+      <div className="hero-section">
+           <div className="hero-section-1">
+                <h1 className="hero-title">EcoCrypt.org</h1>
+                <p className="hero-desc">A social network to enforce enviromental laws</p>
+                <div className="buttons">
+                  <button><span>Learn More</span></button>
+                  <button><span>Get Started</span></button>
+                </div>
+           </div>
+           <div className="hero-section-2">
+             <img src="https://wallpapercave.com/wp/hxU3cUn.jpg" alt="stock photo to be removed"/>
+           </div>
+      </div>
     </div>
   )
 }
