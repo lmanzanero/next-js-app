73f4dd4fdb81dd2dddffe9b9d10c97f015ee6e44
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-02-25 00:36:43 +0800

    added highlights section

--- ui/highlights_section.js
+++ ui/highlights_section.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HighlightsSection() {
   return (
     <div className="section">
-      <h1 className="title">Featured Articles</h1>
+    <h1 className="title">Featured Articles</h1>
      <div className="highlights-section">
         <div className="articles">
           <div className="article">
@@ -13,9 +13,9 @@ export default function HighlightsSection() {
             </h2>
             <p className="author">Author</p>
             <p className="article-exerpt">
-                
+               A riparian forest or riparian woodland is a forested or wooded area of land adjacent to a body of water such as a river, stream, pond, lake, marshland, estuary, canal, sink or reservoir.
             </p>
-            <button>Read More</button>
+            <button><span>Read More</span></button>
           </div>
           <div className="article">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZo_ykGExeA2yrB0PWFqI4mtEbDRsro4P8OQ&usqp=CAU" alt=""/>
@@ -24,9 +24,9 @@ export default function HighlightsSection() {
             </h2>
             <p className="author">Author</p>
             <p className="article-exerpt">
-                
+            A riparian forest or riparian woodland is a forested or wooded area of land adjacent to a body of water such as a river, stream, pond, lake, marshland, estuary, canal, sink or reservoir.
             </p>
-            <button>Read More</button>
+            <button><span>Read More</span></button>
           </div>
           <div className="article">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZo_ykGExeA2yrB0PWFqI4mtEbDRsro4P8OQ&usqp=CAU" alt=""/>
@@ -35,13 +35,13 @@ export default function HighlightsSection() {
             </h2>
             <p className="author">Author</p>
             <p className="article-exerpt">
-                
+            A riparian forest or riparian woodland is a forested or wooded area of land adjacent to a body of water such as a river, stream, pond, lake, marshland, estuary, canal, sink or reservoir.
             </p>
-            <button>Read More</button>
+            <button><span>Read More</span></button>
           </div>
         </div>
-        <button> Read More </button>
      </div>
+     <button><span>Read More</span></button>
     </div>
   )
 }
